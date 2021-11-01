8a98e8f9a0a41c1423b630aacfdd2f21a2d19f11
oassas <oassas@greenflex.com>
2021-11-01 19:17:14 +0100

    add express validator middleware p6-server-05

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect DataBase
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello Express!! 🎉");
 });
